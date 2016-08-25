0d7559a72328fab3ddab582f48002a11b33d3431
Alvin <Alvin@192.168.0.7>
2016-08-25 10:32:13 -0600

    Mantenimiento Lugar Acceso

--- GUIA03_POO2_AlvinBaires/src/main/java/com/sv/udb/controlador/TipoGafeCtrl.java
+++ GUIA03_POO2_AlvinBaires/src/main/java/com/sv/udb/controlador/TipoGafeCtrl.java
@@ -1,22 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.sv.udb.controlador;
 
-import com.sv.udb.modelo.LugaAcce;
+import com.sv.udb.modelo.*;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
-/**
- *
- * @author Laboratorio
- */
 public class TipoGafeCtrl {
-     public boolean guar(LugaAcce obje)
+    
+    public boolean guar(LugaAcce obje)
     {
         boolean resp = false;
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("POOPU");
@@ -35,6 +30,91 @@ public class TipoGafeCtrl {
         }
         em.close();
         emf.close();
+        return resp;
+    }
+    
+     public List<LugaAcce>  ConsTodo()
+    {
+        List<LugaAcce> resp = new ArrayList<>();
+          EntityManagerFactory emf = Persistence.createEntityManagerFactory("POOPU");
+           EntityManager em = emf.createEntityManager();
+        try
+        {
+          TypedQuery<LugaAcce> query =em.createNamedQuery("LugaAcce.findAll", LugaAcce.class);
+           resp = query.getResultList();
+        }
+        catch(Exception ex)
+        {
+            
+        }
+        return resp;
+       
+    }
+        public boolean modi(LugaAcce obje)
+    {
+        boolean resp = false;
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("POOPU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+         LugaAcce lugar = null;
+        tx.begin();
+        try
+        {
+            
+            lugar = em.find(LugaAcce.class, obje.getCodiLugaAcce());
+            lugar.setNombLugaAcce(obje.getNombLugaAcce());
+            tx.commit();
+            resp = true;
+        }
+        catch(Exception ex)
+        {
+            tx.rollback();
+        }
+        em.close();
+        emf.close();
+        return resp;
+    }
+    
+   
+    public boolean elim(Long empId)
+    {
+        boolean resp = false;
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("POOPU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();       
+        tx.begin();
+        LugaAcce respo = null;
+        try{
+            respo = em.find(LugaAcce.class, empId);
+            if(respo != null)
+            {
+                em.remove(respo);
+                tx.commit();
+                resp = true; 
+            }
+        }catch(Exception e){
+            tx.rollback();
+        }
+        em.close();
+        emf.close();
+        return resp;
+    }
+
+
+     
+     
+    public LugaAcce get(Long empId){
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("POOPU");
+        EntityManager em = emf.createEntityManager();
+        LugaAcce resp = null;
+        
+        try{
+            resp = em.find(LugaAcce.class, empId);
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }                
         return resp;
     }
 }
